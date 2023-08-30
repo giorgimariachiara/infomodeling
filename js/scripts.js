8e0a3c8ed6c70aef2680d5f29014173a0472449f
ghasempouri1984 <ash.mathcom@gmail.com>
2023-08-31 00:35:51 +0200

    update accordion menu

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,42 @@ window.addEventListener('DOMContentLoaded', event => {
     listHoursArray[new Date().getDay()].classList.add(('today'));
 })
 
+/*!
+document.addEventListener("DOMContentLoaded", function() {
+    // Get references to the button and the accordion
+    const toggleAccordionButton = document.getElementById("toggleAccordionButton");
+    const accordionExample = document.getElementById("accordionExample");
+
+    // Initially hide the accordion
+    accordionExample.style.display = "none";
+
+    // Add click event listener to the button
+    toggleAccordionButton.addEventListener("click", function() {
+        if (accordionExample.style.display === "none") {
+            accordionExample.style.display = "block";
+        } else {
+            accordionExample.style.display = "none";
+        }
+    });
+});
+*/
+
+/*!
+document.addEventListener("DOMContentLoaded", function() {
+    const toggleAccordionButton = document.getElementById("toggleAccordionButton");
+    const accordionExample = document.getElementById("accordionExample");
+
+    accordionExample.style.display = "none";
+
+    toggleAccordionButton.addEventListener("click", function() {
+        if (accordionExample.style.display === "none") {
+            accordionExample.style.display = "block";
+        } else {
+            accordionExample.style.display = "none";
+        }
+
+        // Scroll to the accordion
+        accordionExample.scrollIntoView({behavior: "smooth"});
+    });
+});
+*/
