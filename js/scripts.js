1d98c4f1010afc56b93885ed21a3f5851a3ad08a
ghasempouri1984 <ash.mathcom@gmail.com>
2023-09-15 12:13:55 +0200

    Styler and Up Buttons
    adding Styler Button and to Top Button

--- js/scripts.js
+++ js/scripts.js
@@ -120,3 +120,10 @@ document.getElementById('stylesBtn').addEventListener('click', function() {
         }
     });
 });
+
+document.getElementById('toTopBtn').addEventListener('click', function() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
