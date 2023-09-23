4aca5d3b55c886dd10dc2dbc9ce7826659e7707c
Maria Chiara <mariachiara.giorgi1@gmail.com>
2023-09-23 16:48:21 +0200

    ,

--- js/tempMap.js
+++ js/tempMap.js
@@ -18,14 +18,68 @@ function setMapTiles(tileUrl, attributionText) {
 
 // For each theme, we have an event listener:
 // Theme1 
+document.querySelector('.sub-btn[data-theme="theme1"]').addEventListener('click', function() {
+    // Switch the CSS
+    document.getElementById('themeStylesheet').href = "css/reinassancefinale.css";
+
+    // Switch the map tiles (for theme5, which is assumed to be the Renaissance theme in this example)
+    setMapTiles(
+        'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+        'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+    );
+});
+
 // Theme2
+document.querySelector('.sub-btn[data-theme="theme2"]').addEventListener('click', function() {
+    // Switch the CSS
+    document.getElementById('themeStylesheet').href = "css/be.css";
+
+    // Switch the map tiles (for theme5, which is assumed to be the Renaissance theme in this example)
+    setMapTiles(
+        'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+        'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+    );
+});
+
 // Theme3
+document.querySelector('.sub-btn[data-theme="theme3"]').addEventListener('click', function() {
+    // Switch the CSS
+    document.getElementById('themeStylesheet').href = "css/issuefuturism.css";
+
+    // Switch the map tiles (for theme5, which is assumed to be the Renaissance theme in this example)
+    setMapTiles(
+        'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+        'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+    );
+});
+
 // Theme4
+document.querySelector('.sub-btn[data-theme="theme4"]').addEventListener('click', function() {
+    // Switch the CSS
+    document.getElementById('themeStylesheet').href = "css/pm.css";
+
+    // Switch the map tiles (for theme5, which is assumed to be the Renaissance theme in this example)
+    setMapTiles(
+        'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+        'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+    );
+});
 // Theme5
-// Theme6
 document.querySelector('.sub-btn[data-theme="theme5"]').addEventListener('click', function() {
     // Switch the CSS
     document.getElementById('themeStylesheet').href = "css/XX.css";
+
+    // Switch the map tiles (for theme5, which is assumed to be the Renaissance theme in this example)
+    setMapTiles(
+        'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+        'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+    );
+});
+
+// Theme6
+document.querySelector('.sub-btn[data-theme="theme6"]').addEventListener('click', function() {
+    // Switch the CSS
+    document.getElementById('themeStylesheet').href = "css/2030.css";
 
     // Switch the map tiles (for theme5, which is assumed to be the Renaissance theme in this example)
     setMapTiles(
